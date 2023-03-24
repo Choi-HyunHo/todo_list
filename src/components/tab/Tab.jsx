c9d4be929a34f010fcf9ed33a155caca24c7ef60
최현호 <x7788@naver.com>
2023-03-25 01:44:40 +0900

    style : tab

--- src/components/tab/Tab.jsx
+++ src/components/tab/Tab.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import style from "./Tab.module.css";
 
-const Tab = ({ filters, onChangeFilter }) => {
-	return (
-		<ul>
-			{filters.map((list, index) => (
-				<li key={index}>
-					<button onClick={() => onChangeFilter(list)}>{list}</button>
-				</li>
-			))}
-		</ul>
-	);
+const Tab = ({ filters, onChangeFilter, filter }) => {
+    return (
+        <div className={style.header}>
+            <ul className={style.filters}>
+                {filters.map((list, index) => (
+                    <li key={index}>
+                        <button
+                            className={`${style.filter} ${
+                                filter === list && style.selected
+                            }`}
+                            onClick={() => onChangeFilter(list)}
+                        >
+                            {list}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Tab;
