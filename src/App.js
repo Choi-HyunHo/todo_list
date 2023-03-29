242d5104d2a0b42d1875bb3cb9f36009352bf016
Choi-HyunHo <x7788@naver.com>
2023-03-29 14:35:40 +0900

    feat : 다크모드 추가 중

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
+import React, { createContext, useCallback, useState } from "react";
 import TodoList from "./components/todoList/TodoList";
 import Tab from "./components/tab/Tab";
+import { useEffect } from "react";
 
+export const DarkModeContext = createContext();
 const filters = ["all", "active", "end"];
 
 function App() {
 	const [filter, setFilter] = useState(filters[0]);
+	const [darkMode, setDarkMode] = useState(false);
+
+	const toggleButton = () => {
+		setDarkMode((mode) => !mode);
+	};
+
+	useEffect(() => {
+		localStorage.setItem("mode", JSON.stringify(darkMode));
+	}, [darkMode]);
 
 	return (
-		<>
+		<DarkModeContext.Provider value={{ darkMode, toggleButton }}>
 			<Tab filters={filters} filter={filter} onChangeFilter={setFilter} />
 			<TodoList filter={filter} />
-		</>
+		</DarkModeContext.Provider>
 	);
 }
 
--- src/components/tab/Tab.jsx
+++ src/components/tab/Tab.jsx
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import style from "./Tab.module.css";
+import { MdDarkMode } from "react-icons/md";
+import { DarkModeContext } from "../../App";
+import { useEffect } from "react";
 
 const Tab = ({ filters, onChangeFilter, filter }) => {
-    return (
-        <div className={style.header}>
-            <ul className={style.filters}>
-                {filters.map((list, index) => (
-                    <li key={index}>
-                        <button
-                            className={`${style.filter} ${
-                                filter === list && style.selected
-                            }`}
-                            onClick={() => onChangeFilter(list)}
-                        >
-                            {list}
-                        </button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+	const { darkMode, toggleButton } = useContext(DarkModeContext);
+
+	const value = JSON.parse(localStorage.getItem("mode"));
+
+	console.log(value);
+
+	return (
+		<div className={style.header}>
+			<MdDarkMode onClick={() => toggleButton()} />
+			<ul className={style.filters}>
+				{filters.map((list, index) => (
+					<li key={index}>
+						<button
+							className={`${
+								darkMode !== true
+									? style.filter
+									: style.darkFilter
+							} ${filter === list && style.selected}`}
+							onClick={() => onChangeFilter(list)}
+						>
+							{list}
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default Tab;
