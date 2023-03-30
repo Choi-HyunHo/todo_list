85010709943ea5903ae7159108d54931a4d88e50
최현호 <x7788@naver.com>
2023-03-30 23:11:08 +0900

    style : 스타일 마무리

--- src/components/tab/Tab.jsx
+++ src/components/tab/Tab.jsx
@@ -1,38 +1,44 @@
 import React from "react";
 import style from "./Tab.module.css";
-import { MdDarkMode } from "react-icons/md";
+import { HiMoon } from "react-icons/hi";
+import { HiSun } from "react-icons/hi";
 import { useSelector, useDispatch } from "react-redux";
 import { modeChange } from "../../store/modeSlice";
 
 const Tab = ({ filters, onChangeFilter, filter }) => {
-	const dispatch = useDispatch();
-	const { value } = useSelector((state) => state.mode);
+    const dispatch = useDispatch();
+    const { value } = useSelector((state) => state.mode);
 
-	if (value === true) {
-		document.documentElement.classList.add("add");
-	} else {
-		document.documentElement.classList.remove("add");
-	}
+    if (value === true) {
+        document.documentElement.classList.add("add");
+    } else {
+        document.documentElement.classList.remove("add");
+    }
 
-	return (
-		<div className={style.header}>
-			<MdDarkMode onClick={() => dispatch(modeChange(!value))} />
-			<ul className={style.filters}>
-				{filters.map((list, index) => (
-					<li key={index}>
-						<button
-							className={`${style.filter} ${
-								filter === list && style.selected
-							}`}
-							onClick={() => onChangeFilter(list)}
-						>
-							{list}
-						</button>
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+    return (
+        <div className={style.header}>
+            <button
+                className={style.toggle}
+                onClick={() => dispatch(modeChange(!value))}
+            >
+                {value === true ? <HiMoon /> : <HiSun />}
+            </button>
+            <ul className={style.filters}>
+                {filters.map((list, index) => (
+                    <li key={index}>
+                        <button
+                            className={`${style.filter} ${
+                                filter === list && style.selected
+                            }`}
+                            onClick={() => onChangeFilter(list)}
+                        >
+                            {list}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Tab;
